025c345e9b0c25d62531b53872cfd41a7489111c
Simon Walker <s.r.walker101@googlemail.com>
2023-03-10 08:07:21 +0000

    Start to build parser tests

--- src/table/parsers/update_item.rs
+++ src/table/parsers/update_item.rs
@@ -4,3 +4,23 @@ use pest::Parser;
 #[derive(pest_derive::Parser)]
 #[grammar = "update_item.pest"]
 struct UpdateItemParser;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple() {
+        for s in &["SET a = b", "SET a = :b", "SET #a = b", "SET #k = :b"] {
+            assert!(UpdateItemParser::parse(Rule::update_expression, s).is_ok());
+        }
+    }
+
+    #[test]
+
+    fn multiple() {
+        for s in &["SET a = b, c = d"] {
+            assert!(UpdateItemParser::parse(Rule::update_expression, s).is_ok());
+        }
+    }
+}
