ecd371d3ff7a315a9290834508668071b7d065e9
Simon Walker <s.r.walker101@googlemail.com>
2023-03-11 18:57:35 +0000

    wip

--- dynamodb-expression-parser/src/parsers/update_item.rs
+++ dynamodb-expression-parser/src/parsers/update_item.rs
@@ -13,14 +13,17 @@ pub enum Node {
 
 #[derive(PartialEq, Debug, Clone)]
 pub struct Assignment {
-    lhs: Name,
+    lhs: Path,
     rhs: Name,
 }
 
 #[derive(PartialEq, Debug, Clone)]
+pub struct Path(Vec<Name>);
+
+#[derive(PartialEq, Debug, Clone)]
 pub enum Name {
     Placeholder(String),
-    Path(Vec<String>),
+    Value(String),
 }
 
 pub fn parse(query: &str) -> Result<Node, ParserError> {
@@ -32,7 +35,7 @@ pub fn parse(query: &str) -> Result<Node, ParserError> {
     assert_eq!(pairs.as_rule(), Rule::update_expression);
     let mut expression = pairs.into_inner().peekable();
     match expression.peek().map(|e| e.as_rule()) {
-        Some(Rule::set_action) => parse_set_action(expression.next().unwrap()),
+        Some(Rule::set_action) => parse_set_actions(expression),
         Some(Rule::remove_action) => todo!(),
         Some(Rule::add_action) => todo!(),
         Some(Rule::delete_action) => todo!(),
@@ -40,7 +43,45 @@ pub fn parse(query: &str) -> Result<Node, ParserError> {
     }
 }
 
-fn parse_set_action(pair: Pair<Rule>) -> Result<Node, ParserError> {
+fn parse_set_actions<'a, I>(items: I) -> Result<Node, ParserError>
+where
+    I: Iterator<Item = Pair<'a, Rule>>,
+{
+    let assignments: Vec<Assignment> = items.map(parse_set_action).collect();
+
+    Ok(Node::Set(assignments))
+}
+
+fn parse_set_action(item: Pair<'_, Rule>) -> Assignment {
+    assert_eq!(item.as_rule(), Rule::set_action);
+    let mut inners = item.into_inner();
+    let lhs = parse_path(inners.next().unwrap());
+    let rhs = parse_value(inners.next().unwrap());
+
+    Assignment { lhs, rhs }
+}
+
+fn parse_path(item: Pair<'_, Rule>) -> Path {
+    assert_eq!(item.as_rule(), Rule::path);
+    // TODO
+    Path(vec![Name::Value("a".to_string())])
+}
+
+fn parse_value(item: Pair<'_, Rule>) -> Name {
+    assert_eq!(item.as_rule(), Rule::value);
+    let pairs: Vec<_> = item.into_inner().collect();
+    if pairs.len() == 1 {
+        // parse_operand(pairs[0])
+        todo!()
+    } else if pairs.len() == 2 {
+        // binop
+        Name::Value("lkjansdg".to_string())
+    } else {
+        unreachable!()
+    }
+}
+
+fn parse_operand(item: Pair<'_, Rule>) -> Name {
     todo!()
 }
 
@@ -56,8 +97,8 @@ mod tests {
         assert_eq!(
             parse(s).unwrap(),
             Node::Set(vec![Assignment {
-                lhs: Name::Path(vec!["a".to_string()]),
-                rhs: Name::Path(vec!["b".to_string()]),
+                lhs: Path(vec![Name::Value("a".to_string())]),
+                rhs: Name::Value("b".to_string()),
             },])
         );
     }
