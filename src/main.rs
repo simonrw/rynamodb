a0368bfec143cdab565567f67bfda1332e525d37
Simon Walker <s.r.walker101@googlemail.com>
2023-03-19 21:22:16 +0000

    Allow custom port for running against tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long, default_value = "3050")]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let args = Args::parse();
+
     let app = rynamodb::router();
-    let port = 3050;
-    tracing::info!(%port, "running server");
-    rynamodb::run_server(app, port).await.unwrap();
+    tracing::info!(%args.port, "running server");
+    rynamodb::run_server(app, args.port).await.unwrap();
 }
