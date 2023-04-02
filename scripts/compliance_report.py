ff2db4588e18a4c5895245b3ef6f44d438de89da
Simon Walker <s.r.walker101@googlemail.com>
2023-04-02 22:02:35 +0100

    Add posting to compliance report server

--- scripts/compliance_report.py
+++ scripts/compliance_report.py
@@ -7,6 +7,9 @@ renders it so that github actions can read it and post it.
 
 
 import argparse
+from datetime import datetime, timezone
+import os
+import requests
 import textwrap
 import xml.sax
 from xml.sax import ContentHandler
@@ -39,6 +42,30 @@ class Visitor(ContentHandler):
         )
         out_stream.write(tpl)
 
+    def post_results(self, secret: str):
+        url = "https://rynamodb-compliance-tracker.simonrw.com/submit"
+
+        now = datetime.now(tz=timezone.utc)
+        payload = {
+            "branch": os.getenv("GITHUB_REF_NAME"),
+            "commitSha": os.getenv("GITHUB_SHA"),
+            "committer": os.getenv("GITHUB_ACTOR"),
+            "errors": self.errors,
+            "failed": self.failures,
+            "skipped": self.skipped,
+            "passed": self.passed,
+            "duration": self.time,
+            "uploaded": now.isoformat(),
+        }
+
+        headers = {
+            "x-rynamodb-token": secret,
+            "content-type": "application/json",
+        }
+
+        r = requests.post(url, json=payload, headers=headers)
+        r.raise_for_status()
+
     @property
     def passed(self) -> int:
         return self.tests - (self.errors + self.failures + self.skipped)
@@ -62,9 +89,12 @@ class Visitor(ContentHandler):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", type=argparse.FileType("r"))
+    parser.add_argument("-s", "--post-secret", help="Submit the results to compliance tracker")
     parser.add_argument("-o", "--output", type=argparse.FileType("w"), default="-")
     args = parser.parse_args()
 
     visitor = Visitor()
     xml.sax.parse(args.filename, visitor)
     visitor.write_to(args.output)
+    if args.post_secret:
+        visitor.post_results(args.post_secret)
