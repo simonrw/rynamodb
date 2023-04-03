238a49091c84d1ac9b0e7ef8af470d822d5b7e85
Simon Walker <s.r.walker101@googlemail.com>
2023-04-03 22:23:45 +0100

    Add compliance roundtrip script

---
+++ scripts/compliance_roundtrip.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+from datetime import datetime, timezone
+from decimal import Decimal
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+ENDPOINT_URL = "http://127.0.0.1:3050"
+dynamodb = boto3.resource("dynamodb", endpoint_url=ENDPOINT_URL)
+
+# create the table
+# TODO: what happens when the same table is created twice?
+table = dynamodb.create_table(
+    TableName="rynamdob",
+    AttributeDefinitions=[
+        dict(
+            AttributeName="branch",
+            AttributeType="S",
+        ),
+        dict(
+            AttributeName="uploaded",
+            AttributeType="S",
+        ),
+    ],
+    KeySchema=[
+        dict(
+            AttributeName="branch",
+            KeyType="HASH",
+        ),
+        dict(
+            AttributeName="uploaded",
+            KeyType="RANGE",
+        ),
+    ],
+)
+table.wait_until_exists()
+
+# insert some data
+# TODO: batch write not implemented
+batch = table
+# with table.batch_writer() as batch:
+batch.put_item(
+    Item={
+        "branch": "main",
+        "uploaded": datetime.now(tz=timezone.utc).isoformat(),
+        "errors": 10,
+        "failed": 3,
+        "skipped": 0,
+        "passed": 200,
+        "duration": Decimal("2.1"),
+        "commit-sha": "randomsha",
+        "committer": "jondoe",
+    }
+)
+batch.put_item(
+    Item={
+        "branch": "main",
+        "uploaded": datetime.now(tz=timezone.utc).isoformat(),
+        "errors": 9,
+        "failed": 2,
+        "skipped": 1,
+        "passed": 199,
+        "duration": Decimal("12.1"),
+        "commit-sha": "randomsha2",
+        "committer": "jondoe",
+    }
+)
+batch.put_item(
+    Item={
+        "branch": "main",
+        "uploaded": datetime.now(tz=timezone.utc).isoformat(),
+        "errors": 13,
+        "failed": 6,
+        "skipped": 3,
+        "passed": 203,
+        "duration": Decimal("42.1"),
+        "commit-sha": "randomsha3",
+        "committer": "jondoe",
+    }
+)
+
+# get the coverage for the main branch
+# Query does not work yet
+res = table.query(
+    KeyConditionExpression=Key("branch").eq("main")
+)
+items = res["Items"]
+print(items)
